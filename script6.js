5a9eeded4c448124a56b228040d47f3de41f393d
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-04-05 02:52:30 +0800

    Day 6 done

--- script6.js
+++ script6.js
@@ -219,12 +219,39 @@ function reverseArray(arr){
     // Im trying to get the last index through let totalIndex = arr.length - 1
     // and then assign it to a variable
     // but idk why its returning []
+    // let totalIndex = arr.length;
+    // for(let i = 0; i < arr.length; i++){
+    //     console.log(totalIndex)
+    //     totalIndex = totalIndex - 1
+    // }
 
+    // Woahhhh ,getting some progress nowwwwwwwwwwwww, I just put the variable totalIndex outside the loop and then decremented it
+    // everytime in the loop, now all I need is to replace the reverse indexes to the totalIndexx value
+    // This hsould be it:
+    
+    // let totalIndex = arr.length;
+    // for(let i = 0; i < arr.length; i++){
+    //     totalIndex = totalIndex - 1
+    //     reverse[totalIndex] = arr[totalIndex]
+    // }
+
+    // Bruh why is it not reverseeedddddddddd, im getting index 5 first rightt????
+
+    // let totalIndex = arr.length;
+    // for(let i = 0; i < arr.length; i++){
+    //     totalIndex = totalIndex - 1
+    //     console.log(arr[totalIndex]);
+    // }
+
+    // Oh okay, so its just correct, but its getting the reverse from the reverse array as well ,  so let me fix this:
+
+    let totalIndex = arr.length;
     for(let i = 0; i < arr.length; i++){
-        let totalIndex = arr.length - 1
-        console.log(totalIndex)
-        totalIndex--
+        totalIndex = totalIndex - 1
+        reverse[i] = arr[totalIndex]
     }
+
+    // OH MY GOD IM LITERALLYYYYYYYYYYYYYYYYYY USING MY LOGIC RIGHT NOW, THE 100% OF MY BRAINNNN WWOWOWOWOWOW
 
     return reverse;
 }
@@ -254,25 +281,163 @@ let stringArr = ['a', 'b', 'c', 'd', 'e']
 
 // This one's workin as well, just needed a second argument
 
-console.log(reverseArray(stringArr))
+// console.log(reverseArray(stringArr))
 
 
 
 // Program 1: Student Analyzer
 
-// let grades = [85, 90, 78, 92, 88]
+let grades = [85, 90, 78, 92, 88]
 
-// function getAverage(arr){
+function getAverage(arr){
 
-//     let total = 0
+    let total = 0
+
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i]
+    }
+
+    let avg = total / arr.length
+
+    return avg;
+}
+
+function getHighest(arr){
+
+    let highest = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > highest){
+            highest = arr[i]
+        }
+    }
+
+    return highest
+}
+
+// function getLowest(arr){
+//     let lowest = 0
 
 //     for(let i = 0; i < arr.length; i++){
-//         total += arr[i]
+//         if(arr[i] < lowest){
+//             lowest = arr[i]
+//         }
 //     }
 
-//     let avg = total / arr.length
-
-//     return avg;
+//     return lowest
 // }
 
+// So this one's pretty tricky as well, cause how do I get a higher number than inside the index, its impossible so i have to look
+// for another way right XD
+
+// function getLowest(arr){
+//     let lowest = 0
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > lowest){
+//             lowest = arr[i]
+//         }
+
+//         if(arr[i] < lowest){
+//             lowest = arr[i]
+//         }
+//     }
+
+//     return lowest
+// }
+
+// This is a  stupid attempt XD
+
+function getLowest(arr){
+    let lowest = arr[0]
+
+    for(let i = 0; i < arr.length; i++){
+
+        if(arr[i] < lowest){
+            lowest = arr[i]
+        }
+    }
+
+    return lowest
+}
+
+// Okay, so I can do this but is this the best answer though?
+// Whatever, as long as it works
+
 // console.log(getAverage(grades));
+// console.log(getHighest(grades));
+// console.log(getLowest(grades));
+
+
+
+
+
+
+
+
+
+
+// Program 2: Simple Inventory System
+
+// let fruits = ["apple", "banana", "mango"]
+// let input = prompt('Enter a fruit: ')
+
+// function searchItem(arr, input){
+
+//     // input = prompt('Enter a fruit: ')
+//     // Can I do this or do I need to declare it outside the reusable function?
+//     // If I have to declare it outside, then itll always ask for something everytime the code runs then
+//     // Whatever, let me try this
+//     // Yea, I have to decalre it outside,t whatever
+    
+//     for(let i = 0; i < arr.length; i++){
+//         if(input === arr[i]){
+//             return input + ' is found';
+//         }
+//     }
+
+//     return input + ' is not found'
+// }
+
+// console.log(searchItem(fruits, input))
+
+
+// Program 2 [Add Item] (BRUH YOU DIDNT EVEN TEACH ME HOW TO ADD AN ITEM WTTFFF, OR WHAT FUNCTION BRUH WHATEVER)
+
+// let fruits = ["apple", "banana", "mango"]
+// let userInput = prompt('Enter a fruit to add: ')
+
+function addItem(array, input){
+
+    let totalLength = array.length
+    // let idx = array.length - 1
+
+    // Bruh how do I insert another index
+
+    array[totalLength] = input
+
+    return array;
+}
+
+// console.log(addItem(fruits, userInput))
+
+// BRUH HOW DID I EVEN GET THIS RIGHTTTTTTT THE FIRST TIME OMGOGMGOMGOMGOMGG EZEEZEZEZEZEZ
+
+
+// Q1: [1, 2, 3, 4]
+// Q2: [1, 2]
+// Q3: 10
+// Q4: 
+
+// function sum(arr) {
+//     let total = 0;
+
+//     for(let i = 0; i < arr.length; i++) {
+//         total += arr[i];
+//     }
+
+//     return total;
+// }
+
+
+
