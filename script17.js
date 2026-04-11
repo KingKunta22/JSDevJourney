724ff32afad10bf9f46715093d61278a03172522
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-04-11 12:21:27 +0800

    Completely lost now at day 17

--- script17.js
+++ script17.js
@@ -2,11 +2,11 @@ const prompt = require('prompt-sync')()
 
 // Redo Program 1 from Day 16
 
-const cart = [
-  { name: "Dog Food", price: 500, qty: 2 },
-  { name: "Cat Toy", price: 150, qty: 1 },
-  { name: "Dog Leash", price: 300, qty: 3 }
-];
+// const cart = [
+//   { name: "Dog Food", price: 500, qty: 2 },
+//   { name: "Cat Toy", price: 150, qty: 1 },
+//   { name: "Dog Leash", price: 300, qty: 3 }
+// ];
 
 // Add item (if exists → increase qty, else → add new)
 
@@ -138,8 +138,153 @@ const cart = [
 
 // console.log(getTotalQty)
 
+// const cart = [
+//   { name: "Dog Food", price: 500, qty: 2 },
+//   { name: "Cat Toy", price: 150, qty: 1 },
+//   { name: "Dog Leash", price: 300, qty: 3 }
+// ];
 
-// Program 1: Easy
+// Program 1: Guided
+
+// let userInput = prompt('Add a product: ')
+
+// const findProduct = cart.find(item => item.name.toLowerCase() === userInput.toLowerCase())
+// So this is working, moving on...
+
+// I still don't know why we have to add a new cart, is there no way you can just append the new item, create its new properties then add to cart?
+// But since we're doing mutable and immutable topic, I think that's the reason why
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         item.qty += 1;
+//         console.log('Item added successfully')
+//         return item
+//     })
+// }
+// console.log(cart)
+
+// Wow, I need lots of practice and familiarity with array methods, because until now, I still don't know how this is wrong
+
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         if(item.name.toLowerCase() === findProduct.name.toLowerCase()){
+//             item.qty += item
+//             return item
+//         }
+//         return item
+//     })
+// }
+// console.log(cart)
+
+
+// SEEEE, HOW AM I STILL CONFUSED BRO, DO I NEED A FUCKIGN ENTIRE WEEK OF PRACTICE TO DO THIS ARRAY METHODS SHIT
+
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         if(item.name.toLowerCase() === findProduct.name.toLowerCase()){
+//             item.qty += 1
+//             return item
+//         }
+//         return item
+//     })
+// } else {
+//     return newCart = [
+//         {
+//             ...cart,
+//             name: findProduct,
+//             price: 0,
+//             qty: 1
+//         }
+//     ]
+// }
+
+// Okay, now this is kind of working, we now need to add a newCart
+// I still don't get the idea of newCart
+// console.log(cart)
+
+// I am soooo fucking lost and confused, specifically the newCart part qwurybvhqinjorkmpl;,
+
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         if(item.name.toLowerCase() === findProduct.name.toLowerCase()){
+//             item.qty += 1
+//             return item
+//         }
+//         return item
+//     })
+// } else {
+//     return newCart = [
+//             ...cart,
+//         {
+//             name: findProduct,
+//             price: 0,
+//             qty: 1
+//         }
+//     ]
+// }
+
+// BROOOOOOOOOOOOOOOOOOOOO
+
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         if(item.name.toLowerCase() === findProduct.name.toLowerCase()){
+//             item.qty += 1
+//             return item
+//         }
+//         return item
+//     })
+// } else {
+//     newCart = [
+//             ...cart,
+//         {
+//             name: userInput,
+//             price: 0,
+//             qty: 1
+//         }
+//     ]
+//     return newCart
+// }
+
+// console.log(cart)
+// console.log(newCart)
+
+// Okay, I might need to copy the original and set the newCart value for either true or false inside the condtion
+
+// let newCart;
+
+// if(findProduct) {
+//     const addQty = cart.map((item) => {
+//         if(item.name.toLowerCase() === findProduct.name.toLowerCase()){
+//             item.qty += 1
+//             return newCart = {...cart}
+//         }
+//          return newCart = [{...cart}]
+//     })
+// } else {
+//     newCart = [
+//             ...cart,
+//         {
+//             name: userInput,
+//             price: 0,
+//             qty: 1
+//         }
+//     ]
+//     return newCart
+// }
+
+// console.log(cart)
+// console.log(newCart)
+
+// I AM SOOOOO FUCKING LOST MAN
 
 
 
@@ -147,3 +292,99 @@ const cart = [
 
 
 
+
+
+
+
+
+
+
+
+// Program 2: Unguided
+
+const cart = [
+  { name: "Dog Food", price: 500, qty: 2 },
+  { name: "Cat Toy", price: 150, qty: 1 },
+  { name: "Dog Leash", price: 300, qty: 3 },
+  { name: "Bird Food", price: 100, qty: 10 }
+];
+
+
+// const removeItems = cart.filter(item => item.qty < 2)
+
+// Wow, I mean this is 100000x easier than the Program 1 even if its guided, I am just, I guess, confused with the newCart, why we have to
+// mutable or immutable it or whatever u call that, I am completely lost on that part
+
+// const applyDiscount = cart.map(item => item.price -= 50)
+
+// const getTotalPrice = cart.reduce((total, item) => {
+    // total += item.qty * item.price
+// }, 0)
+
+// console.log(cart)
+
+// // So, right now, the functions aren't being applied to the cart yet, do we need to call it or what
+
+// console.log(removeItems, applyDiscount, getTotalPrice)
+// console.log(cart)
+
+// Oh okay, I might need to put everything in one function or if else or whatever
+
+// let newCart2 = cart.filter((item) => {
+//     if(item.qty >= 2){
+//         // This will get all the items above 2 quantity, so we need to map those then here
+//         // Then apply the discounts
+//         newCart2 = cart.map((item) => {
+//             item.price -= item.price - 50;
+//             // Then get total price here
+//             let total = cart.reduce((total, item) => {
+//                 total += item.price * item.qty
+//             }, 0)
+//             console.log('Total Price: ' + total)
+//             console.log(cart)
+//             console.log(newCart2)
+//             return newCart2
+//         })        
+//     }
+// })
+
+// Well, hopefully it works but I am doubtful, okay it didnt, as expected
+
+let newCart2;
+
+const doAll = cart.filter((item) => {
+    if(item.qty >= 2){
+        // This will get all the items above 2 quantity, so we need to map those then here
+        // Then apply the discounts
+        newCart2 = cart.map((item) => {
+            item.price -= item.price - 50;
+            // Then get total price here
+            let total = cart.reduce((total, item) => {
+                total += item.price * item.qty
+            }, 0)
+            console.log('Total Price: ' + total)
+            return {...newCart2 = cart}
+        })        
+    }
+})
+
+console.log(doAll)
+
+// YEP, I AM FUCKING LOST, I WANT YOU TO ANALYZE EVERYTHING AND TEACH ME
+// I AM COMPLETELY LOST, UNEDUCATED, AND CONFUSED ABOUT THE {...} thing, the newCart, the immutability and mutability, and the returning shit
+
+
+// DEBUGGING: 
+const newCart = cart.map(item => {
+  if (item.price > 300) {
+    return { ...cart, price: item.price - 50 };
+  }
+  return item;
+});
+
+
+// REFLECTION
+
+// 1. Idk because separating it makes me understand how the program works?
+// 2. I am good with some of array methods, just immutability, mutability, returning shit, and coimbining everything and referencing, and copyhing, and quwbntojqkmp
+// 3. NOTHING
