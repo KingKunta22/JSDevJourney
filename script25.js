70a69b87f1f889931fda4dc406e2d79c23eba7c5
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-02 13:58:51 +0800

    Day 26: 2 more big bugs left

--- script25.js
+++ script25.js
@@ -4,7 +4,12 @@ const prompt = require('prompt-sync')()
 
 // I am gonna follow your rules and will look for help if I get stuck on this stuff after a day XD
 
-let cart = []
+let cart = [
+  { name: "Dog Food", price: 500, qty: 2 },
+  { name: "Cat Toy", price: 150, qty: 1 },
+  { name: "Dog Leash", price: 300, qty: 3 },
+  { name: "Bird Food", price: 100, qty: 10 }
+];
 
 // CLI CART SYSTEM - DATA LAYER
 
@@ -17,6 +22,14 @@ function view(cart){
 function add(cart, targetItem){
     let exists = cart.find(item => item.name.toLowerCase() === targetItem.toLowerCase())
 
+    // if(!Number.isFinite(targetPrice) || typeof targetPrice !== 'number') {
+    //     console.log(targetPrice)
+    //     console.log(typeof targetPrice)
+    //     return { error: 'Invalid price'}
+    // }
+    // What's wrong with this? This just returns the targetPrice to undefined....
+    // Oh nevermind, nothing';s wrong with that but whatever...
+
     if(exists){
         cart = cart.map(item => {
             if(item.name.toLowerCase() === targetItem.toLowerCase()){
@@ -26,10 +39,11 @@ function add(cart, targetItem){
         })
         return cart
     } else {
+        let targetPrice = Number(prompt('Input price: '))
         let updatedCart = [...cart, {
             name: targetItem,
-            qty: 1,
-            price: 0
+            price: targetPrice,
+            qty: 1
         }]
         return updatedCart
     }
@@ -58,13 +72,13 @@ function update(cart, targetItem, targetQty){
     let exists = cart.find(item => item.name.toLowerCase() === targetItem.toLowerCase())
 
     if(exists){
-        cart = cart.map(item => {
+        let updatedCart = cart.map(item => {
             if(item.name.toLowerCase() === targetItem.toLowerCase()){
                 return {...item, qty: targetQty}
             }
             return item
         })
-        return cart
+        return updatedCart
     } else {
         return {error: 'Item does not exist'}
     }
@@ -73,7 +87,7 @@ function update(cart, targetItem, targetQty){
 // Show Total
 function total(cart){
     let total = cart.reduce((sum, item) => {
-        return sum + item.price * item.qty
+        return sum += item.price * item.qty
     }, 0)
     
     return total
@@ -81,44 +95,210 @@ function total(cart){
 
 // Clear Cart
 function clear(cart){
-    cart = []
+    // cart = []
+    // return cart // BRUH HOW IS THIS NOT EVEN WORKING....
+    return []
 }
 
-// Exit
-function exit(){ // What do u even do with this XDDDD, nvm ill figure it out later
+// // Exit
+// function exit(){ // What do u even do with this XDDDD, nvm ill figure it out later
     
-}
+// }
+
+
+// // CLI CART SYSTEM - ENGINE
+// function cartEngine(cart, action, payload){
+
+//     if(action === 'add'){
+//         return {status: 'success', data: add(cart, payload)}
+//     }
+
+//     if(action === 'remove'){
+//         let result = remove(cart, payload)
+//         if(result.error) return {status: 'error', message: error.message}
+//         return {status: 'success'}
+//     }
+
+//     if(action === 'update'){
+//         let result = update(cart, payload.name, payload.qty)
+//         if(result.error) return {status: 'error', message: error.message}
+//         return {status: 'success'}
+//     }
+
+//     if(action === 'total'){
+//         return {status: 'success', data: total(cart)}
+//     }
+
+//     if(action === 'clear'){
+//         return {status: 'success', data: clear(cart)}
+//     }
+
+//     // if(action === 'exit'){
+
+//     // }
+
+//     return {error: 'Invalid action'}
+// }
+
+
+// // CLI CART SYSTEM - HANDLER
+
+// // function handleRequest() 
+// // Okay, so do I need this still? I don't think soo....
+
+// let running = true // Well, new stuff learned, this looks smart instead of js doing while(true)
+
+// while(running){
+//     console.log("\n=== CLI SYSTEM ===")
+//     console.log("1. View Cart")
+//     console.log("2. Add Item")
+//     console.log("3. Remove Item")
+//     console.log("4. Update Item")
+//     console.log("5. Clear Cart")
+//     console.log("6. Get Total")
+//     console.log("7. Exit")
+//     let userInput = prompt("Choose: ")
+
+//     if(userInput === '1'){
+//         // Okay, what do I even put hereeee, nvm, i had to look up your shit to get some ideas t-t
+//         console.log(cart)
+//     }
+
+//     if(userInput === '2'){
+//         let item = prompt('Add Item: ')
+//         cart = cartEngine(cart, 'add', item)
+//     }
+
+//     if(userInput === '3'){
+//         let item = prompt('Remove item: ')
+//         cart = cartEngine(cart, 'remove', item)
+//     }
+
+//     if(userInput === '4'){
+//         let item = prompt('Update an item: ')
+//         let qty = Number(prompt('Enter quantity: '))
+//         cart = cartEngine(cart, 'update', item, qty)
+//     }
+
+//     if(userInput === '5'){
+//         // console.log(total(cart)) // Oh, I should use the cartEngine
+//         console.log(cartEngine(cart, 'total')) //I hope that's right
+//     }
+
+//     if(userInput === '6'){
+//         console.log(cartEngine(cart, 'clear'))
+//     }
+    
+//     if(userInput === '7'){
+//         running = false
+//         console.log('Goodbye!')
+//     }
+// }
+
+// Well, there are a lot of bugs
+// 1. Why is it showing success only when I view the cart after adding an item? Shouldnt it show the success or  result after adding the item?
+// 2. Adding a second item shows an error: TypeError: cart.find is not a function
+//                                        at add (C:\Users\Leonard\Documents\PERSONAL\Codes\JSTest\script25.js:18:23)
+// 3. Get total even after adding an item is undefined (should be because I didn't ask the user to add a price...)
+
+// Okay, I see where most of these problems are coming from:
+
+// RIGHT NOW, IF I ADD AN ITEM IT TURNS CART INTO > { status: 'success', data: [ { name: 'Dog food', qty: 1, price: 0 } ] }
+// Instead of just { name: 'Dog food', qty: 1, price: 0 }
+// I believe this is the reason why it's not reading the cart as an object with just name, qty, and price
+// Because I tried jhust doing:
+        // return add(cart, payload)
+// On the cart engine and it worked fine...
+
+// And also, what's the point of adding result on the cartEngine? I can just call the data functions and then just use view cart if it successfully
+// worked right??? Let me fix most of it by copying and pasting the thing I did above but this time with the fixes (so u can see the progress)
 
 
 // CLI CART SYSTEM - ENGINE
 function cartEngine(cart, action, payload){
 
     if(action === 'add'){
-        return {status: 'success', data: add(cart, payload)}
+        return add(cart, payload)
     }
 
     if(action === 'remove'){
-        let result = remove(cart, payload)
-        if(result.error) return {status: 'error', message: error.message}
-        return {status: 'success'}
+        return remove(cart, payload)
     }
 
     if(action === 'update'){
-        let result = update(cart, payload.name, payload.qty)
-        if(result.error) return {status: 'error', message: error.message}
-        return {status: 'success'}
+        return update(cart, payload.name, payload.qty)
+    }
+
+    if(action === 'total'){
+        return total(cart)
     }
 
     if(action === 'clear'){
-        return {status: 'success', data: clear(cart)}
+        return clear(cart)
     }
 
-    if(action === 'exit'){
-        
-    }
+    // if(action === 'exit'){
+
+    // }
+
+    return {error: 'Invalid action'}
 }
 
 
 // CLI CART SYSTEM - HANDLER
 
-let running = true // Well, new stuff learned, this looks smart instead of js doing while(true)
+let running = true
+
+while(running){
+    console.log("\n=== CLI SYSTEM ===")
+    console.log("1. View Cart")
+    console.log("2. Add Item")
+    console.log("3. Remove Item")
+    console.log("4. Update Item")
+    console.log("5. Get Total")
+    console.log("6. Clear Cart")
+    console.log("7. Exit")
+    let userInput = prompt("Choose: ")
+
+    if(userInput === '1'){
+        console.log(cart)
+    }
+
+    if(userInput === '2'){
+        let item = prompt('Add Item: ')
+        // let price = Number(prompt('Input Price: ')) // I think this is misplaced, I need to put it on the data function
+        cart = cartEngine(cart, 'add', item)
+    }
+
+    if(userInput === '3'){
+        let item = prompt('Remove item: ')
+        cart = cartEngine(cart, 'remove', item)
+    }
+
+    if(userInput === '4'){
+        let item = prompt('Update an item: ')
+        let qty = Number(prompt('Enter quantity: '))
+        // cart = cartEngine(cart, 'update', payload: {item: name, qty: qty}) // Okay, I am kinda confused here and I am quite certain this is one of the problem
+        // cart = cartEngine(cart, 'update', name, qty) 
+        console.log(cartEngine(cart, 'update', {item: item, qty: qty}))
+        cart = cartEngine(cart, 'update', {item: item, qty: qty}) // BRUHHHH, THIS HSOUDL BE CORRECT, CUS THIS IS HOW IT SHOULD BE BASED FROM THE PREV STUFF
+    }
+
+    if(userInput === '5'){
+        console.log(cartEngine(cart, 'total'))
+    }
+
+    if(userInput === '6'){
+        console.log(cartEngine(cart, 'clear'))
+    }
+    
+    if(userInput === '7'){
+        running = false
+        console.log('Goodbye!')
+    }
+}
+
+// Okay, new errors, the add, remove, total, and exit functions are perfectly working (AFAIK...)
+// However, the problems are in the two functioms: clear and update
+// When I update an item, it doesnt work and returns an error that doesn't even give me the reason why its not working
+// And when I clear, it just returns [], but when I view cart, it shows the cart that nothing changed... and not even cleared
