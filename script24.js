937ef42dafb146918f45338b0a7c3ab9a68a9a3a
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-02 10:54:40 +0800

    Day 24: Submission

--- script24.js
+++ script24.js
@@ -199,13 +199,125 @@ function update(cart, targetItem, targetQty){
 
 // WOAH, OKAY, I JUST REALIZED I HAD TO CHANGE THE CART ENGINE AND THE HANDLER... LET ME REDO IT BNASED FROM YOUR CODE...
 
-// (NEW) ENGINE LAYER
+// // (NEW) ENGINE LAYER
 
+// function cartEngine(cart, action, payload){
+//     if(action === 'add'){
+//         // On this part, why not same with the remove one? Adding something like this:
+//         // let result = add(cart, payload)
+//         // if(result.error) return { status: 'error', message: result.error}
+//         return {status: 'success', data: add(cart, payload)}
+//     } 
+
+//     if(action === 'remove'){
+//         let result = remove(cart, payload)
+//         if(result.error) return {status: 'error', message: result.error}
+//         return {status: 'success', data: result}
+//     }
+
+//     if(action === 'update'){
+//         let result = update(cart, payload.name, payload.qty)
+//         if(result.error) return {status: 'error', message: result.error}
+//         return {status: 'success', data: result}
+//     }
+
+//     return {status: 'error', message: 'Invalid Action'}
+// }
+
+// // Now, I kinda understand how this works now, it just simplifies everything and adds a new feature where it shows an error
+// // The next one (handle request) is where I dont udnerstand as much...
+
+// // HANDLER
+
+// function handleRequest(cart, requests){
+//     for(let req of requests){ // Question, is this the same as foreach? like using foreach(items as item??)????
+
+//         let result = cartEngine(cart, req.action, req.payload)
+
+//         if(result.status === 'success'){
+//             cart = result.data
+//         } else {
+//             console.log('Error:', result.message)
+//         }
+//     } 
+//     return cart
+// }
+
+// YEP, I AM LOST AND CONFUSED ON THAT PART...
+
+// Let me try it though...
+
+// let cart = [
+//   { name: "Dog Food", price: 500, qty: 2 }
+// ]
+
+// let requests = [
+//   { action: "add", payload: "Dog Food" },
+//   { action: "update", payload: { name: "Dog Food", qty: 5 } },
+//   { action: "remove", payload: "Dog Food" }
+// ]
+
+// console.log(handleRequest(cart, requests))
+// WOW, ITS WORKING, BUT I NEED SOME KIND OF CLARIFICATION ABOUT WHAT HAPPENED SPECIALLY ON THE HANDLER REQUEST PART, BUT WHATEVER,
+// THE MORE I DO IT , THE MORE I SHOULD UNDERSTAND IT....
+
+
+// Okay, so by total and clear action, you mean create functions for both actios right? bnecause actions = functions
+
+// Task 1: Create a 'Clear' function/action
+
+// So, I assume this just deletes everything in the cart without the option to revert the changes
+function clear(cart){
+    let updatedCart = cart.filter(item => {
+        // Hmm, you gave me an idea on how to do this efficiently but I forgot, I'll just make sure the condition always return false
+        if(false){
+            return
+        }
+    })
+    return updatedCart
+}
+
+// console.log(clear(cart))
+// Well, that didn't work.... why is the first obj returning true?
+// Ohh, it was working, I just had this uncommented -> console.log(handleRequest(cart, requests)) XDDDD
+
+// Okay, great, working on first try
+
+
+// Task 2: Total
+
+// Since you added 2500 as data, I assume that's the user's cash
+function total(cart, targetCash){
+    if(!Number.isFinite(targetCash) || typeof targetCash !== 'number'){
+        return {error: 'Invalid cash input'}
+    }
+
+    let total = cart.reduce((sum, item) => {
+        return sum + item.price * item.qty
+    }, 0)
+
+    if(targetCash < total) {
+        return {error: 'Insufficient balance'}
+    }
+
+    let change = targetCash - total
+
+    return {
+        item: cart,
+        cash: targetCash,
+        total: total,
+        change: change
+    }
+}
+
+// console.log(total(cart, 5000))
+// Okay wowowowowow, perfect working on first tryyyy, but I have some error data on my function total so I have to put it onto the engine cart
+// I'll also just put everything below so that the it follows the proper layering (Data Layer > Engine Layer > Handler)
+
+
+// ENGINE LAYER
 function cartEngine(cart, action, payload){
     if(action === 'add'){
-        // On this part, why not same with the remove one? Adding something like this:
-        // let result = add(cart, payload)
-        // if(result.error) return { status: 'error', message: result.error}
         return {status: 'success', data: add(cart, payload)}
     } 
 
@@ -220,15 +332,23 @@ function cartEngine(cart, action, payload){
         if(result.error) return {status: 'error', message: result.error}
         return {status: 'success', data: result}
     }
+    if(action === 'clear'){
+        // let result = clear(cart)
+        // if(result.error)
+            // Oh shi-, I think I don't need an error handler because there's no error inside my fclear function...
+        return {status: 'success', data: clear(cart)}
+    }
+    if(action === 'total'){
+        let results = total(cart, payload)
+        if(results.error) return {status: 'error', message: results.error}
+        return {status: 'success', data: results}
+    }
 
     return {status: 'error', message: 'Invalid Action'}
 }
 
-// Now, I kinda understand how this works now, it just simplifies everything and adds a new feature where it shows an error
-// The next one (handle request) is where I dont udnerstand as much...
 
 // HANDLER
-
 function handleRequest(cart, requests){
     for(let req of requests){ // Question, is this the same as foreach? like using foreach(items as item??)????
 
@@ -243,10 +363,8 @@ function handleRequest(cart, requests){
     return cart
 }
 
-// YEP, I AM LOST AND CONFUSED ON THAT PART...
 
-// Let me try it though...
-
+// CHECK IF WORKING:
 let cart = [
   { name: "Dog Food", price: 500, qty: 2 }
 ]
@@ -254,9 +372,22 @@ let cart = [
 let requests = [
   { action: "add", payload: "Dog Food" },
   { action: "update", payload: { name: "Dog Food", qty: 5 } },
-  { action: "remove", payload: "Dog Food" }
+  { action: "remove", payload: "Dog Food" },
+//   { action: "clear"}
+  // Okay, the clear is working, I believe, but I don't want to add it to the requests array
+//   { action: "total", payload: 5000}
+  // So this is not working on first try, I believe because of this line:
+    //   if(isNaN(targetCash) || typeof targetCash !== 'number'){
+    //     return {error: 'Invalid cash input'}
+    // }
+    // Al;right, so I looked it up online, asked for the best error handler and should only accept numbers, it seems like it doesnt recommend the isNan
+    // -function and recommends this instead !Number.isFinite(targetCash), it mentioned that thats the golden standard upon checking if an input is cash
+    // -value, so i'll js follow it
+    // It also gave me a 'pro tip' to convert targetCash to Number so that it accepts this "2500" and 2500 at the same time, I kinda dont want that though
+    // I want it to only accept 2500, but in like realworld shit, do they do that? Anyways, lmsee if its working
+  { action: "total", payload: 5000}
+  // BRUH, THIS STILL WASN'T OWORKING UNTIL I FOUND THE BUG > payload.cash, LOOOLLL, I SHOULDVE JUST USED PAYLOAD BECAUSE IT ONLY ACCEPTS CART AND 
+  // ANOTHER ARGUMENT, CONTRARY TO UPDATE function WHICH ACCEPTS 3 INPUTS OR ARGUMENTS
 ]
 
 console.log(handleRequest(cart, requests))
-// WOW, ITS WORKING, BUT I NEED SOME KIND OF CLARIFICATION ABOUT WHAT HAPPENED SPECIALLY ON THE HANDLER REQUEST PART, BUT WHATEVER,
-// THE MORE I DO IT , THE MORE I SHOULD UNDERSTAND IT....
