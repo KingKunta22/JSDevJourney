fb128fb8d7d55492c7086585d8333b00d186a195
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-08 19:55:11 +0800

    Conditional Rendering

--- react-app-yt/src/components/ListGroup.tsx
+++ react-app-yt/src/components/ListGroup.tsx
@@ -8,10 +8,15 @@ const items = [
     'Naga'
 ]
 
+function getMessage() {
+    return <p>Sample Message</p>
+}
+
 function ListGroup(){
     return (
         <>
-            <h1 className='text-4xl font-bold'>List</h1>
+            <h1 className='text-4xl font-bold'>List Group</h1>
+            {items.length === 0 && getMessage()}
             <ul className="w-64 rounded-lg border border-zinc-200 bg-white divide-y divide-zinc-200 overflow-hidden">
                 {items.map(item => <li key={item}>{item}</li>)}
             </ul>
