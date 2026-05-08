29c51dd40db8f763bb41417702fe7b44850d1e5e
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-08 08:08:18 +0800

    Learning bout fragments

--- react-app-yt/src/components/ListGroup.tsx
+++ react-app-yt/src/components/ListGroup.tsx
@@ -1,11 +1,16 @@
+// import { Fragment } from 'react';
+
 function ListGroup(){
     return (
-    <ul className="w-64 rounded-lg border border-zinc-200 bg-white divide-y divide-zinc-200 overflow-hidden">
-        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 1</li>
-        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 2</li>
-        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 3</li>
-        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 4</li>
-    </ul>
+        <>
+            <h1 className='text-4xl font-bold'>List</h1>
+            <ul className="w-64 rounded-lg border border-zinc-200 bg-white divide-y divide-zinc-200 overflow-hidden">
+                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 1</li>
+                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 2</li>
+                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 3</li>
+                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 4</li>
+            </ul>
+        </>
     )
 }
 
