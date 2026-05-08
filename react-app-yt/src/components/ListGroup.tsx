eed00d1674e6cf33a802cce021d30a4c0b104a83
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-08 19:45:52 +0800

    Rendering Lists done

--- react-app-yt/src/components/ListGroup.tsx
+++ react-app-yt/src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 // import { Fragment } from 'react';
 
+const items = [
+    'Cebu',
+    'Makati',
+    'Metro Manila',
+    'Davao',
+    'Naga'
+]
+
 function ListGroup(){
     return (
         <>
             <h1 className='text-4xl font-bold'>List</h1>
             <ul className="w-64 rounded-lg border border-zinc-200 bg-white divide-y divide-zinc-200 overflow-hidden">
-                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 1</li>
-                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 2</li>
-                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 3</li>
-                <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 4</li>
+                {items.map(item => <li key={item}>{item}</li>)}
             </ul>
         </>
     )
