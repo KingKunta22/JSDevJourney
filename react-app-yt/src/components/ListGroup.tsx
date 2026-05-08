2ded4b45b097405dc39b0fe144c93a2c0ef75138
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-08 08:04:09 +0800

    Doing this w tw

---
+++ react-app-yt/src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup(){
+    return (
+    <ul className="w-64 rounded-lg border border-zinc-200 bg-white divide-y divide-zinc-200 overflow-hidden">
+        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 1</li>
+        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 2</li>
+        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 3</li>
+        <li className="px-4 py-2 hover:bg-zinc-50 cursor-pointer">Item 4</li>
+    </ul>
+    )
+}
+
+export default ListGroup
