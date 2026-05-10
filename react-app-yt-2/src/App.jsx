114f57cedd1b062c6943463e6b11e9152e2d9d5a
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-10 18:04:40 +0800

    New boring component

--- react-app-yt-2/src/App.jsx
+++ react-app-yt-2/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './Header.jsx'
+import Food from './Food.jsx'
 import Footer from './Footer.jsx'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return(
     <>
       <Header/>
+      <Food/>
       <Footer/>
     </>
   )
