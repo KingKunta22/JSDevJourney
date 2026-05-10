8045d235da3ae37ae94d98a0e5108f6664332e4e
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-10 19:19:11 +0800

    Fixed it without AI

--- react-app-yt-2/src/Card.jsx
+++ react-app-yt-2/src/Card.jsx
@@ -3,7 +3,7 @@ import profilePic from './assets/download.jpg'
 function Card(){
     return(
         <div className="card">
-            <img src="{profilePic}" alt="Profile Picture"></img>
+            <img className="card-image" src={ profilePic } alt="Profile Picture" ></img>
             <h2 className="card-title">Leo Inferior</h2>
             <p className="card-text">I am a noobbb</p>
         </div>
