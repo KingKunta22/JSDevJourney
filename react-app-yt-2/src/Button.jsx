472a97d110903d23a710ee54617b4e1b344c37ce
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-11 16:20:27 +0800

    Sending arguments to a function

--- react-app-yt-2/src/Button.jsx
+++ react-app-yt-2/src/Button.jsx
@@ -1,7 +1,10 @@
 function Button() {
     const handleClick = () => { console.log('OUCH!')}
+    function handleClick2(name) {
+        console.log('Hello ' + name)
+    }
 
-    return(<button onClick={handleClick}>Click me!!</button>)
+    return(<button onClick={() => handleClick2('Leo')}>Click me!!</button>)
 }
 
 export default Button
