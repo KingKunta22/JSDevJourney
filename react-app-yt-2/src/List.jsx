e5a266332b3d7f66980a1b3330cf26d655dd4a5d
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-11 11:41:59 +0800

    Learned about array method sort

--- react-app-yt-2/src/List.jsx
+++ react-app-yt-2/src/List.jsx
@@ -7,7 +7,16 @@ function List(){
         {id: 4, name: 'peach', calories: 45}, 
         {id: 5, name: 'strawberry', calories: 67}
     ]
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>)
+
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL ORDER
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSED ALPHAB ORDER
+    // fruits.sort((a, b) => a.calories - b.calories) // NUMERIC ORDER
+    fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC ORDER
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+        {fruit.name}: &nbsp;
+        <b>{fruit.calories}</b>
+        </li>)
 
     return(<ol>{listItems}</ol>)
 }
