fd1cce1f2a603c0f3510d6e2a69438df086c9c70
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-11 11:52:32 +0800

    Created a reusable component

--- react-app-yt-2/src/App.jsx
+++ react-app-yt-2/src/App.jsx
@@ -8,8 +8,27 @@ import List from './List.jsx'
 
 function App() {
   
+    const fruits = [
+        {id: 1, name: 'apple', calories: 51}, 
+        {id: 2, name: 'mango', calories: 60}, 
+        {id: 3, name: 'watermelon', calories: 105}, 
+        {id: 4, name: 'peach', calories: 45}, 
+        {id: 5, name: 'strawberry', calories: 67}
+    ]
+
+    const vegetables = [
+        {id: 1, name: 'carrots', calories: 8}, 
+        {id: 2, name: 'cabbage', calories: 5}, 
+        {id: 3, name: 'tomatoes', calories: 10}, 
+        {id: 4, name: 'squash', calories: 4}, 
+        {id: 5, name: 'broccoli', calories: 6}
+    ]
+
   return(
-    <List />
+    <>
+      <List items={fruits} category="Fruits"/>
+      <List items={vegetables} category="Vegetables"/>
+    </>
   )
 
 }
--- react-app-yt-2/src/List.jsx
+++ react-app-yt-2/src/List.jsx
@@ -1,12 +1,12 @@
-function List(){
+function List(props){
 
-    const fruits = [
-        {id: 1, name: 'apple', calories: 51}, 
-        {id: 2, name: 'mango', calories: 60}, 
-        {id: 3, name: 'watermelon', calories: 105}, 
-        {id: 4, name: 'peach', calories: 45}, 
-        {id: 5, name: 'strawberry', calories: 67}
-    ]
+    // const fruits = [
+    //     {id: 1, name: 'apple', calories: 51}, 
+    //     {id: 2, name: 'mango', calories: 60}, 
+    //     {id: 3, name: 'watermelon', calories: 105}, 
+    //     {id: 4, name: 'peach', calories: 45}, 
+    //     {id: 5, name: 'strawberry', calories: 67}
+    // ]
 
     // fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL ORDER
     // fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSED ALPHAB ORDER
@@ -16,9 +16,11 @@ function List(){
     // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
     // const highCalFruits = fruits.filter(fruit => fruit.calories > 100)
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-        {fruit.name}: &nbsp;
-        <b>{fruit.calories}</b>
+    const itemList = props.items
+
+    const listItems = itemList.map(item => <li key={item.id}>
+        {item.name}: &nbsp;
+        <b>{item.calories}</b>
         </li>)
 
     return(<ol>{listItems}</ol>)
