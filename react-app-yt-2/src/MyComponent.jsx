7767cd588f3448daf3c5afd4182c1d25cc7235be
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-12 22:12:53 +0800

    Still learning useState

--- react-app-yt-2/src/MyComponent.jsx
+++ react-app-yt-2/src/MyComponent.jsx
@@ -3,15 +3,27 @@ import React, {useState} from 'react'
 function MyComponent() {
 
     const [name, setName] = useState('Guest')
+    const [age, setAge] = useState(0)
+
     const updateName = () => {
         setName('Leo')
     }
 
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
     return(
-        <div>
-            <p>Name: {name}</p>
-            <button onClick={updateName}>Set Name</button>
-        </div>
+        <>
+            <div>
+                <p>Name: {name}</p>
+                <button onClick={updateName}>Set Name</button>
+            </div>
+            <div>
+                <p>Age: {age}</p>
+                <button onClick={incrementAge}>Increment</button>
+            </div>
+        </>
     )
 }
 
