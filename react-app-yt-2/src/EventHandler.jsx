eaaefd0319dfbcd4140d49783bc0411e0ce6db36
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-12 22:42:38 +0800

    Idk wtf is happening ngl

--- react-app-yt-2/src/EventHandler.jsx
+++ react-app-yt-2/src/EventHandler.jsx
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 
 function EventHandler() {
 
-    
+    const [name, setName] = useState('')
 
     return(
-
+        <div>
+            <input value={name} />
+        </div>
     )
 }
 
